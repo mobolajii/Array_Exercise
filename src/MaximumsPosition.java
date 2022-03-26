962c6f8dc7943bc13e4d33312160bd8287ff299e
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 18:25:01 +0100

    This returns the largest number in the array.

--- src/MaximumsPosition.java
+++ src/MaximumsPosition.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class Maximum {
-
-    //returns the biggest number in the array
-
-    static int maximum(int[]data){
+public class MaximumsPosition {
+    //prints the index of maximum
+    static void maximumsPosition(int[]data){
         int max = data[0];
         for(int i=0; i<data.length; i++){
             if(data[i]>max)max = data[i];
-
+            if(max == data[i]) System.out.println(i);
         }
-        return max;
+
     }
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int limit = sc.nextInt();
@@ -23,6 +19,6 @@ public class Maximum {
             data[i] = sc.nextInt();
         }
 
-        System.out.println(maximum(data));
+        maximumsPosition(data);
     }
 }
