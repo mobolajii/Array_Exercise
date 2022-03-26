0a14030efb42cc21a41ed9b75fdfa36982de7ad9
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 16:59:33 +0100

    This counts how often index is the same as data[index].

---
+++ src/Often.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Often{
+
+    //counts how often index is the same as data[index]
+    static int often(int[]data){
+        int count = 0;
+
+        for(int i=0; i< data.length; i++){
+            if(data[i] == i)count++;
+        }
+        return count;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int limit = sc.nextInt();
+        int[]data = new int[limit];
+
+        for(int i=0; i<limit; i++){
+            data[i] = sc.nextInt();
+        }
+
+        System.out.println(often(data));
+    }
+}
