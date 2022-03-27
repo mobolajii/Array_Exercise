775947a9cc78dfcfe6294268fdff023f42efd2f9
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-27 17:37:43 +0200

    Returns second largest number in the array.

---
+++ src/SecondMaximum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+// Java program for the above approach
+class SecondMaximum {
+
+    static int secondmax(int[]data){
+
+        int max = data[0];
+        int secondMax = data[0];
+
+        for(int i=0; i<data.length; i++){
+            if(data[i]>max) {
+                secondMax = max;
+                max = data[i];
+            }else if(data[i]>secondMax)secondMax = data[i];
+
+        }
+
+        return secondMax;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int limit = sc.nextInt();
+        int[]data = new int[limit];
+
+        for(int i=0; i<data.length; i++){
+            data[i] = sc.nextInt();
+        }
+
+        System.out.println(secondmax(data));
+    }
+}
+
